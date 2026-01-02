17117d00e05665cb01095dc0320b6626c548fede
euro20179 <Euro20171@proton.me>
2026-01-02 15:58:54 -0800

    update

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #endif
 
-#define LANGUAGE_VERSION 15
+#define LANGUAGE_VERSION 14
 #define STATE_COUNT 434
 #define LARGE_STATE_COUNT 83
 #define SYMBOL_COUNT 85
@@ -2815,7 +2815,7 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
   }
 }
 
-static const TSLexerMode ts_lex_modes[STATE_COUNT] = {
+static const TSLexMode ts_lex_modes[STATE_COUNT] = {
   [0] = {.lex_state = 0, .external_lex_state = 1},
   [1] = {.lex_state = 63, .external_lex_state = 2},
   [2] = {.lex_state = 63, .external_lex_state = 3},
@@ -14723,7 +14723,6 @@ TS_PUBLIC const TSLanguage *tree_sitter_mmfml(void) {
     .state_count = STATE_COUNT,
     .large_state_count = LARGE_STATE_COUNT,
     .production_id_count = PRODUCTION_ID_COUNT,
-    .supertype_count = SUPERTYPE_COUNT,
     .field_count = FIELD_COUNT,
     .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
     .parse_table = &ts_parse_table[0][0],
@@ -14750,13 +14749,6 @@ TS_PUBLIC const TSLanguage *tree_sitter_mmfml(void) {
       tree_sitter_mmfml_external_scanner_deserialize,
     },
     .primary_state_ids = ts_primary_state_ids,
-    .name = "mmfml",
-    .max_reserved_word_set_size = 0,
-    .metadata = {
-      .major_version = 0,
-      .minor_version = 1,
-      .patch_version = 0,
-    },
   };
   return &language;
 }
